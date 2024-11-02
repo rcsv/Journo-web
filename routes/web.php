5dfb5b33f50dcfdf817bf98655acaa9fe033520a
Tomohiro AWANE <Awane.Tomohiro@gmail.com>
2024-11-03 01:17:47 +0900

    ログインユーザーだけに Trip 関連のコントロールアクセスができるように Auth ミドルウェアの支配下に移動

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,35 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TripController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::middleware('guest')->group(function(){
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+
+Route::middleware(['web','auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Journo Application
+    Route::resource('/trip', TripController::class);
 });
 
 require __DIR__.'/auth.php';
 
 // Journo アプリケーションのルーティング情報は下記のファイルで管理
-require __DIR__ . '/journo.php' ;
+//require __DIR__ . '/journo.php' ;
+
